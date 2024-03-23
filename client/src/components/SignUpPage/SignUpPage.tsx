96f9264851aa8c153c1327af36dc14e49384af32
James Olsen <olsenjames1116@gmail.com>
2024-03-23 17:49:02 -0400

    Set up sign up page with basic inputs

--- client/src/components/SignUpPage/SignUpPage.tsx
+++ client/src/components/SignUpPage/SignUpPage.tsx
@@ -1,5 +1,28 @@
+import { useEffect } from 'react';
+
 function SignUpPage() {
-	return <div>SignUpPage</div>;
+	useEffect(() => {
+		document.title = 'Sign Up';
+	});
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log('sign up form submit');
+	};
+
+	return (
+		<main>
+			<div>
+				<img src="" alt="" />
+				<span>App Title</span>
+				<form method="POST" onSubmit={handleSubmit}>
+					<input type="text" />
+					<input type="password" />
+					<button>Sign Up</button>
+				</form>
+			</div>
+		</main>
+	);
 }
 
 export default SignUpPage;
