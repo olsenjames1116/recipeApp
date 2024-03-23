d6ae889ac20d03bd1408c206f43bc7b907316b22
James Olsen <olsenjames1116@gmail.com>
2024-03-23 18:27:22 -0400

    Create event handler for input change in sign up page

--- client/src/components/SignUpPage/SignUpPage.tsx
+++ client/src/components/SignUpPage/SignUpPage.tsx
@@ -1,6 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function SignUpPage() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
 	useEffect(() => {
 		document.title = 'Sign Up';
 	});
@@ -10,14 +14,51 @@ function SignUpPage() {
 		console.log('sign up form submit');
 	};
 
+	const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+		const { id, value } = target;
+
+		switch (id) {
+			case 'username':
+				setUsername(value);
+				console.log(value);
+				break;
+			case 'password':
+				setPassword(value);
+				console.log(value);
+				break;
+			case 'confirmPassword':
+				setConfirmPassword(value);
+				console.log(value);
+				break;
+			default:
+				console.log('None of the ids matched');
+		}
+	};
+
 	return (
 		<main>
 			<div>
 				<img src="" alt="" />
 				<span>App Title</span>
 				<form method="POST" onSubmit={handleSubmit}>
-					<input type="text" />
-					<input type="password" />
+					<input
+						id="username"
+						type="text"
+						placeholder="username"
+						onChange={handleChange}
+					/>
+					<input
+						id="password"
+						type="password"
+						placeholder="password"
+						onChange={handleChange}
+					/>
+					<input
+						id="confirmPassword"
+						type="password"
+						placeholder="confirm password"
+						onChange={handleChange}
+					/>
 					<button>Sign Up</button>
 				</form>
 			</div>
