17ac6756c886a8d16846c23e8d2cbb77c3490136
James Olsen <olsenjames1116@gmail.com>
2024-03-24 14:04:12 -0400

    Set up front end validation

--- client/src/components/SignUpForm/SignUpForm.tsx
+++ client/src/components/SignUpForm/SignUpForm.tsx
@@ -1,8 +1,9 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import api from '../../axiosConfig';
 
 // Represents the sign up form to create a new user.
 function SignUpForm() {
+	const [inputMessages, setInputMessages] = useState<string[]>([]);
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
@@ -10,6 +11,10 @@ function SignUpForm() {
 	const usernameRef = useRef<HTMLInputElement>(null);
 	const passwordRef = useRef<HTMLInputElement>(null);
 	const confirmPasswordRef = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		console.log(inputMessages);
+	}, [inputMessages]);
 
 	// Send input to the backend.
 	const signUp = async () => {
@@ -20,6 +25,7 @@ function SignUpForm() {
 				confirmPassword: confirmPassword,
 			});
 
+			// This is reached if all input is valid.
 			console.log(response);
 		} catch (error) {
 			console.log(error);
@@ -32,12 +38,66 @@ function SignUpForm() {
 		if (confirmPasswordRef.current) confirmPasswordRef.current.value = '';
 	};
 
+	// Reached when there is an error detected from front end validation.
+	const handleInputError = () => {
+		// Each check will determine what caused the error and display the appropriate error message for clarity.
+		if (!usernameRef.current?.checkValidity()) {
+			usernameRef.current?.validity.valueMissing &&
+				setInputMessages((state) => [...state, 'Username must not be empty.']);
+
+			usernameRef.current?.validity.tooLong &&
+				setInputMessages((state) => [
+					...state,
+					'Username must be less than 20 characters.',
+				]);
+		}
+
+		if (!passwordRef.current?.checkValidity()) {
+			passwordRef.current?.validity.valueMissing &&
+				setInputMessages((state) => [...state, 'Password must not be empty.']);
+
+			passwordRef.current?.validity.tooLong &&
+				setInputMessages((state) => [
+					...state,
+					'Password must be less than 20 characters.',
+				]);
+		}
+
+		if (!confirmPasswordRef.current?.checkValidity()) {
+			confirmPasswordRef.current?.validity.valueMissing &&
+				setInputMessages((state) => [
+					...state,
+					'Confirmation password must not be empty.',
+				]);
+
+			confirmPasswordRef.current?.validity.tooLong &&
+				setInputMessages((state) => [
+					...state,
+					'Confirmation password must be less than 20 characters.',
+				]);
+		}
+
+		if (confirmPasswordRef.current?.value !== passwordRef.current?.value) {
+			setInputMessages((state) => [...state, 'Passwords must match.']);
+		}
+
+		clearInput();
+	};
+
 	// Reached when the form has been submitted.
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
-		console.log('sign up form submit');
-		clearInput();
-		signUp();
+		setInputMessages([]);
+
+		if (
+			!event.currentTarget.checkValidity() ||
+			confirmPasswordRef.current?.value !== passwordRef.current?.value
+		) {
+			handleInputError();
+		} else {
+			// If input is valid, the below will execute.
+			signUp();
+		}
 	};
 
 	// Reached when a change has been made to an input field.
@@ -61,13 +121,15 @@ function SignUpForm() {
 	};
 
 	return (
-		<form method="POST" onSubmit={handleSubmit}>
+		<form method="POST" onSubmit={handleSubmit} noValidate>
 			<input
 				id="username"
 				type="text"
 				placeholder="username"
 				onChange={handleChange}
 				ref={usernameRef}
+				required
+				maxLength={20}
 			/>
 			<input
 				id="password"
@@ -75,6 +137,8 @@ function SignUpForm() {
 				placeholder="password"
 				onChange={handleChange}
 				ref={passwordRef}
+				required
+				maxLength={20}
 			/>
 			<input
 				id="confirmPassword"
@@ -82,6 +146,8 @@ function SignUpForm() {
 				placeholder="confirm password"
 				onChange={handleChange}
 				ref={confirmPasswordRef}
+				required
+				maxLength={20}
 			/>
 			<button>Sign Up</button>
 		</form>
