1c7ba20a3aaa5bf22f57c323f6b2a9f1f22f0529
James Olsen <olsenjames1116@gmail.com>
2024-04-01 18:40:06 -0400

    Seach recipe list using query

--- client/src/components/MealPlannerForm/MealPlannerForm.tsx
+++ client/src/components/MealPlannerForm/MealPlannerForm.tsx
@@ -1,17 +1,24 @@
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../redux/store';
+
 // Represents the form to search stored recipes to add to planner.
 function MealPlannerForm() {
-	const searchForRecipe = (
-		event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-	) => {
-		event.preventDefault();
+	const recipeList = useSelector((state: IRootState) => state.recipeList.value);
 
-		console.log('search recipe');
+	// Search recipes stored by user for recipe using query.
+	const searchRecipes = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const query = event.target.value;
+
+		const recipeSearchResults = recipeList.filter((recipe) =>
+			recipe.title?.toLowerCase().includes(query.toLowerCase())
+		);
+
+		console.log(recipeSearchResults);
 	};
 
 	return (
 		<form>
-			<input type="text" />
-			<button onClick={searchForRecipe}>Search</button>
+			<input type="text" onChange={searchRecipes} />
 		</form>
 	);
 }
