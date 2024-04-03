4d360c15f38a75c6e447ef41fa0742adfcc29d6a
James Olsen <olsenjames1116@gmail.com>
2024-04-03 10:56:35 -0400

    Clear out redux on log out

--- client/src/components/LogOut/LogOut.tsx
+++ client/src/components/LogOut/LogOut.tsx
@@ -1,8 +1,36 @@
 import { useNavigate } from 'react-router-dom';
 import api from '../../axiosConfig';
+import { useDispatch } from 'react-redux';
+import { removeRecipeType } from '../../redux/state/recipeTypeSlice';
+import { removeAllIngredients } from '../../redux/state/allIngredientsSlice';
+import { removeUserIngredients } from '../../redux/state/userIngredientsSlice';
+import { removeRecipeList } from '../../redux/state/recipeListSlice';
+import { removeSelectedDay } from '../../redux/state/selectedDaySlice';
+import { removePlanner } from '../../redux/state/plannerSlice';
+import { removeGroceries } from '../../redux/state/groceryListSlice';
+import { removeRecipesQuery } from '../../redux/state/recipesQuerySlice';
+import { removeRandomRecipe } from '../../redux/state/randomRecipeSlice';
+import { removeRecipeSearchResults } from '../../redux/state/recipeSearchResultsSlice';
+import { removeSearchIngredients } from '../../redux/state/searchIngredientsSlice';
 
 function LogOut() {
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	// Clears out state stored in Redux.
+	const clearRedux = () => {
+		dispatch(removeRandomRecipe());
+		dispatch(removeRecipeType());
+		dispatch(removeAllIngredients());
+		dispatch(removeUserIngredients());
+		dispatch(removeSearchIngredients());
+		dispatch(removeRecipeList());
+		dispatch(removeSelectedDay());
+		dispatch(removeRecipesQuery());
+		dispatch(removeRecipeSearchResults());
+		dispatch(removePlanner());
+		dispatch(removeGroceries());
+	};
 
 	// Remove user's credentials from the backend.
 	const logUserOut = async (
@@ -13,7 +41,7 @@ function LogOut() {
 		try {
 			await api.delete('/user/log-out');
 
-			// Reached after a successful call from the backend.
+			clearRedux();
 			navigate('/log-in');
 		} catch (error) {
 			// A catch all for errors produced from api call.
