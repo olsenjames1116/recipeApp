3444404ab9701c220ac62bbbe060e998ee6232d1
James Olsen <olsenjames1116@gmail.com>
2024-04-20 17:42:53 -0400

    Setup testing

---
+++ client/src/components/SignUpForm/__test__/SignUpForm.test.tsx
@@ -0,0 +1,28 @@
+// import SignUpForm from '../SignUpForm';
+// import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { describe, it, expect } from 'vitest';
+
+describe('SignUpForm', () => {
+	// it('should display success message if all input is correct', async () => {
+	// 	render(<SignUpForm />);
+
+	// });
+	// function setup(component: JSX.Element) {
+	// 	return {
+	// 		user: userEvent.setup(),
+	// 		...render(component),
+	// 	};
+	// }
+
+	it('should render sign up button', () => {
+		// render(<SignUpForm />);
+
+		// const signUpButton = screen.getByRole('button');
+
+		expect(1 + 1).toBe(2);
+	});
+	// should display an error message if username is empty
+	// should display an error message if password is empty
+	// should display an error message if passwords do not match
+});
