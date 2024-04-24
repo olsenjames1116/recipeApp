7c61cc11397280cc57fc69c9cb0ce2f05b4c2e00
James Olsen <olsenjames1116@gmail.com>
2024-04-24 17:00:32 -0400

    Unit tests for sign up routes and controllers

--- server/__test__/app.test.ts
+++ server/__test__/app.test.ts
@@ -1,6 +1,95 @@
 import { app } from '../app';
+import User from '../models/user';
 const request = require('supertest');
 
-describe('app', () => {
-	it('should pass', () => {});
+describe('Routes and Controllers', () => {
+	beforeAll(async () => {
+		await User.findOneAndDelete({ username: 'testUser2' });
+	});
+
+	describe('Happy Path', () => {
+		it('should validate a user and store a user', (done) => {
+			request(app)
+				.post('/user/sign-up')
+				.send({
+					username: 'testUser2',
+					password: '123',
+					confirmPassword: '123',
+				})
+				.expect(201, done);
+		});
+	});
+
+	describe('Error Path', () => {
+		describe('Username Errors', () => {
+			it('should return an error if username is empty.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({ password: '123', confirmPassword: '123' })
+					.expect(400, done);
+			});
+			it('should return an error if username already exists in database.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({
+						username: 'demo',
+						password: '123',
+						confirmPassword: '123',
+					})
+					.expect(400, done);
+			});
+			it('should return an error if username is too long.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({
+						username: 'qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm',
+						password: '123',
+						confirmPassword: '123',
+					})
+					.expect(400, done);
+			});
+		});
+		describe('Password Errors', () => {
+			it('should return an error if password is empty.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({ username: 'testUser3', confirmPassword: '123' })
+					.expect(400, done);
+			});
+
+			it('should return an error if passwords are too long.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({
+						username: 'testUser3',
+						password: 'qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm',
+						confirmPassword:
+							'qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm',
+					})
+					.expect(400, done);
+			});
+		});
+		describe('Confirmation Password Errors', () => {
+			it('should return an error if confirmation password is empty.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({
+						username: 'testUser3',
+						password: '123',
+					})
+					.expect(400, done);
+			});
+
+			it('should return an error if passwords do not match.', (done) => {
+				request(app)
+					.post('/user/sign-up')
+					.send({
+						username: 'testUser3',
+						password: '123',
+						confirmPassword: '456',
+					})
+					.expect(400, done);
+			});
+		});
+	});
 });
